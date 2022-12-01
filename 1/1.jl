b838a5745eedf12b903712d8880d96af24229da8
Tor S. Haugland <tor.s.haugland@ntnu.no>
2022-12-01 06:50:59 +0100

    Update

--- 1/1.jl
+++ 1/1.jl
@@ -1,20 +1,27 @@
-function f(x)
-    # Parse input
-    elf = []
-    weight = 0
-    for line in readlines("1/1.txt")
-        if line == ""
-            append!(elf, weight)
-            weight = 0
-        else
-            weight += parse(Int, line)
+# Split vector by delimiter
+import Base.split
+function split(x::AbstractVector, dlm)
+    loc = findall(==(dlm), x)
+    getindex.(Ref(x), UnitRange.([1; loc .+ 1], [loc .- 1; length(x)]))
+end
+
+function f(fname)
+    lines = readlines(fname)
+
+    all_calories = zeros(Int, length(lines))
+    for (i, line) in enumerate(lines)
+        try
+            all_calories[i] = parse(Int, line)
+        catch # line == ""
+            all_calories[i] = -1
         end
     end
-    append!(elf, weight)
 
+    elf_calories = sum.(split(all_calories, -1))
+    
     # Calculate 
-    @show maximum(elf)
-    @show sum(sort(elf)[end-2:end])
+    @show maximum(elf_calories)
+    @show sum(sort(elf_calories)[end-2:end])
 end
 
 f("1/1.txt");
