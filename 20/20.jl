d10ee86d9331307cdc957c14af26f43e110ee9d7
Tor S. Haugland <tor.s.haugland@ntnu.no>
2022-12-23 14:27:39 +0100

    20

--- 20/20.jl
+++ 20/20.jl
@@ -1,20 +1,63 @@
-lines = readlines("20/test.txt")
-A = zeros(Int, length(lines))
-B = 1:length(lines) |> collect
-for (i, line) in enumerate(lines)
-    A[i] = parse(Int, line)
+using CircularArrays
+
+function parse_input(fname)
+    lines = readlines(fname)
+    A = zeros(Int, length(lines))
+    B = 1:length(lines) |> collect
+    for (i, line) in enumerate(lines)
+        A[i] = parse(Int, line)
+    end
+
+    A = CircularVector(A)
+    B = CircularVector(B)
+    return A, B
 end
 
-function move(A, i, n)
-    dir = sign(n)
-    amt = abs(n)
-    key = A[i]
-    C = copy(A)
-    while amt > 0
-        amt -= 1
-        C[i + amt*dir + dir] = C[i + amt*dir]
+function move(B, i, t)
+    times = t % (length(B)-1)
+    temp = B[i]
+    if times >= 0
+        for j = i:i+times-1
+            B[j] = B[j+1]
+        end
+        B[i+times] = temp
+    else
+        for j = i:-1:i+times
+            B[j] = B[j-1]
+        end
+        B[i+times] = temp
     end
-    amt += 1
-    C[i + amt*dir] = key
-    return C
 end
+
+function solve_part1()
+    A, B = parse_input("20/input.txt")
+    for i = 1:length(A)
+        n = findfirst(==(i), B)
+        x = A[n]
+        move(A, n, x)
+        move(B, n, x)
+    end
+    n0 = findfirst(==(0), A)
+    @show A[n0 + 1000] + A[n0 + 2000] + A[n0 + 3000]
+end
+
+
+# PART 2
+function solve_part2()
+    A, B = parse_input("20/input.txt")
+    decryption_key = 811589153
+    A *= decryption_key
+    for _ = 1:10
+        for i = 1:length(A)
+            n = findfirst(==(i), B)
+            x = A[n]
+            move(A, n, x)
+            move(B, n, x)
+        end
+    end
+    n0 = findfirst(==(0), A)
+    @show A[n0 + 1000] + A[n0 + 2000] + A[n0 + 3000]
+end
+
+solve_part1()
+solve_part2()
